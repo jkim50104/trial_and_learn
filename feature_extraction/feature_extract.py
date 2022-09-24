1091fcb7a7e511be346e7108e5887ef4975599ee
jkim50104 <45681657+jkim50104@users.noreply.github.com>
2022-09-24 22:17:42 +0900

    Add files via upload

---
+++ feature_extraction/feature_extract.py
@@ -0,0 +1,44 @@
+import torch
+import torch.nn as nn
+import torchvision.models as models
+import torchvision.transforms as transforms
+from torch.autograd import Variable
+from PIL import Image
+
+# Load the pretrained model
+model = models.resnet18(pretrained=True)
+
+# Use the model object to select the desired layer
+layer = model._modules.get('avgpool')
+
+# Set model to evaluation mode
+model.eval()
+
+# Image transforms
+scaler = transforms.Resize((224, 224))
+normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+to_tensor = transforms.ToTensor()
+
+def get_vector(image_name):
+    # 1. Load the image with Pillow library
+    img = Image.open(image_name)
+    # 2. Create a PyTorch Variable with the transformed image
+    t_img = Variable(normalize(to_tensor(scaler(img))).unsqueeze(0))
+    # 3. Create a vector of zeros that will hold our feature vector
+    #    The 'avgpool' layer has an output size of 512
+    my_embedding = torch.zeros(512)
+    # 4. Define a function that will copy the output of a layer
+    def copy_data(m, i, o):
+        my_embedding.copy_(o.data.reshape(o.data.size(1)))
+    # 5. Attach that function to our selected layer
+    h = layer.register_forward_hook(copy_data)
+    # 6. Run the model on our transformed image
+    model(t_img)
+    # 7. Detach our copy function from the layer
+    h.remove()
+    # 8. Return the feature vector
+    return my_embedding.numpy()
+
+image_feature = get_vector(image_name="D:\python files\CNN\CNN_Fruit_Class/train\Apple/0_100.jpg")
+print(image_feature)
+print(image_feature.shape)
---
+++ feature_extraction/test.py
@@ -0,0 +1,9 @@
+from numpy import zeros
+
+
+x = [1,2]
+y = [2,3,5]
+x+y
+
+print(x+y)
+
